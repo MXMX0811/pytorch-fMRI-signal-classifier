5da656d31a78de34986de479694ba7e8e7ef183a
nkMengXin <39915426+nkMengXin@users.noreply.github.com>
2020-11-11 12:04:11 +0800

    Update CNN-LSTM_iftarg_AllBrain.py

--- CNN-LSTM_iftarg_AllBrain.py
+++ CNN-LSTM_iftarg_AllBrain.py
@@ -101,9 +101,9 @@ def dataReader(sub_list, task_list):
                     cond[i] = 0
                 elif cond[i] == 'lure_hard':
                     cond[i] = 1
+            del cond[24]   # 最后一段时间不全
             label.extend(cond)
             label = list(map(int,label))
-            del label[24]   # 最后一段时间不全
 
             data = img[:,:,:,12:]   # 从第13个时间点开始，删除前12个时间点
 
